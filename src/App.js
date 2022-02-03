ffecb4bc546596baa50c5e8a133023be777b3fb4
Jake <jakewarrenblack01@gmail.com>
2022-02-03 17:11:46 +0000

    Looking at useReducer for managing checkbox state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
+// considering how we might manage a checkBox's state using React
+
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  // second argument = dependency array
-  // if nothing passed, props in the state will always have their initial values
-
-  // but can also use it to keep track of values,
-  // eg here we pass emotion so useEffect will update if state changes
-
-  // this will watch emotion change
-  useEffect(() => {
-    console.log(`It's ${emotion} around here`);
-  }, [emotion]);
-
-  // this will watch secondary change
-  useEffect(() => {
-    console.log(`It's also ${secondary} around here!`);
-  }, [secondary]);
+  // a reducer function = receive the current state, and return a new state
+  // useReducer receives two arguments: the reducer function, and the initial state
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
   return (
     <>
-      <h1>
-        Current mood is {emotion} and {secondary}
-      </h1>
-      {/* Plug a new value into the 'emotion' state */}
-      <button onClick={() => setEmotion("happy")}>happy</button>
-      <button onClick={() => setEmotion("sad")}>sad</button>
-      <button onClick={() => setEmotion("frustrated")}>frustrated</button>
-      <button onClick={() => setSecondary("crabby")}>crabby</button>
+      {/* We now use our reducer function to update the state of the checkbox */}
+      <input type="checkbox" value={checked} onChange={toggle} />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
