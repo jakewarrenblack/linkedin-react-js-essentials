a0537651b3f6f94b8e16497db0c195b1a961a88c
Jake <jakewarrenblack01@gmail.com>
2022-02-03 14:48:41 +0000

    How to handle images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+// First import image as a variable
+import restaurant from "./restaurant.jpg";
 
 // A component is a function that returns UI
 // Wherever the component is, that's where you add props
@@ -12,12 +14,16 @@ function Header(props) {
 
 const dishes = ["Bananas", "Coddle", "Stew"];
 
+// Convert to objects, use the value and its index, means we can use the index as a key
+// Better to do this instead of padding 'dish, i' as the key in the component itself,
+// can get jumbled if we add or remove from the array
 const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
 
 function Main(props) {
   return (
     <section>
       <p>We serve the most {props.adjective} food around</p>
+      <img src={restaurant} height={200} alt="Table at a restaurant" />
       <ul style={{ listStyle: "none" }}>
         {props.dishes.map((dish) => (
           <li key={dish.id}>{dish.title}</li>
