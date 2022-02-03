049d4bb3ec7157c75a0a3c4726bb7edf1b51a866
Jake <jakewarrenblack01@gmail.com>
2022-02-03 15:14:28 +0000

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import "./App.css";
-// First import image as a variable
-import restaurant from "./restaurant.jpg";
 
-// A component is a function that returns UI
-// Wherever the component is, that's where you add props
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Top secret information here</h1>;
 }
 
-const dishes = ["Bananas", "Coddle", "Stew"];
-
-// Convert to objects, use the value and its index, means we can use the index as a key
-// Better to do this instead of padding 'dish, i' as the key in the component itself,
-// can get jumbled if we add or remove from the array
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around</p>
-      <img src={restaurant} height={200} alt="Table at a restaurant" />
-      <ul style={{ listStyle: "none" }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function NormalComponent() {
+  return <h1>Anybody can see this Component</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <h3>Copyright {props.year}</h3>
-    </footer>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="John" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+  return <>{props.authorised ? <SecretComponent /> : <NormalComponent />}</>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-function AppTwo() {
-  return <h1>This is the second app</h1>;
-}
-
 ReactDOM.render(
-  // React.Fragment helps us get around using an unnecessary <div> tag,
-  // just for wrapping our two apps. Like in Vue, we need one root component.
-  // The fragment acts like a wrapper without actually adding anything to the DOM
-  // Our </> is just shorthand for 'React.Fragment'
-  <>
-    <App />,
-    <AppTwo />
-  </>,
+  <App authorised={false} />,
+
   document.getElementById("root")
 );
