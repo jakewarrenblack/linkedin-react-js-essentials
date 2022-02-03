4a653e3fe744a90893d184a8d4458c33723cfa75
Jake <jakewarrenblack01@gmail.com>
2022-02-03 14:28:18 +0000

    Learning to create components and use props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// A component is a function that returns UI
+// Wherever the component is, that's where you add props
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <h3>Copyright {props.year}</h3>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="John" />
+      <Main adjective="amazing" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  /* Name of tag, properties to apply, child */
+  // React.createElement(
+  //   "h1",
+  //   {
+  //     style: { color: "blue" },
+  //   },
+  //   "Hello world"
+  // ),
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // ** Can have a tag as a child, but this is really repetitive... **
+  // React.createElement(
+  //   "ul",
+  //   null,
+  //   React.createElement("li", null, "Monday"),
+  //   React.createElement("li", null, "Tuesday"),
+  //   React.createElement("li", null, "Wednesday"),
+  //   React.createElement("li", null, "Thursday")
+  // ),
+
+  // Better like this, JSX syntax
+  // Remember this doesn't run in the browser natively, Babel takes care of it,
+  // converts this to use the above 'create element' calls
+  // <ul>
+  //   <li>Monday</li>
+  //   <li>Tuesday</li>
+  //   <li>Wednesday</li>
+  //   <li>Thursday</li>
+  // </ul>,
+
+  <App />,
+
+  document.getElementById("root")
+);
