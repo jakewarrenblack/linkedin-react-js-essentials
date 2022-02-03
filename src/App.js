dd8fedbc42097c0d7c5397dab9e2ebb5d27b9ae4
Jake <jakewarrenblack01@gmail.com>
2022-02-03 16:53:51 +0000

    A quick look at useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  // We can use array destructuring to pop the first item out of this array
-  // and give it a name
-
-  // useState has 2 values in it, so we use destructuring to get at them,
-  // instead of having to use indexes
-
-  // first is state variable, second is function for updating the state
-  // this is the naming convention:
   const [emotion, setEmotion] = useState("happy");
-  // useState(initial value here, can be anything; boolean, number, string)
+  const [secondary, setSecondary] = useState("tired");
+
+  // second argument = dependency array
+  // if nothing passed, props in the state will always have their initial values
+
+  // but can also use it to keep track of values,
+  // eg here we pass emotion so useEffect will update if state changes
+
+  // this will watch emotion change
+  useEffect(() => {
+    console.log(`It's ${emotion} around here`);
+  }, [emotion]);
+
+  // this will watch secondary change
+  useEffect(() => {
+    console.log(`It's also ${secondary} around here!`);
+  }, [secondary]);
 
   return (
     <>
-      <h1>Current mood is {emotion}</h1>
+      <h1>
+        Current mood is {emotion} and {secondary}
+      </h1>
       {/* Plug a new value into the 'emotion' state */}
       <button onClick={() => setEmotion("happy")}>happy</button>
       <button onClick={() => setEmotion("sad")}>sad</button>
       <button onClick={() => setEmotion("frustrated")}>frustrated</button>
+      <button onClick={() => setSecondary("crabby")}>crabby</button>
     </>
   );
 }
